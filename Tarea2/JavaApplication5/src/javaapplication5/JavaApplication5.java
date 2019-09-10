a862f66604f9ce6589dfa44a4c1da678108c6602
erick <mazae2000@estudiantec.cr>
2019-09-09 22:04:37 -0600

    inicio

---
+++ Tarea2/JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,17 @@
+
+package javaapplication5;
+
+/**
+ *
+ * @author Erick
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
